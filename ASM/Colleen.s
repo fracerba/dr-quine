e36958efd95234716fe775e5d16476abe82b4f8b
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2026-05-19 11:35:51 +0200

    Finished dr-quine

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -2,7 +2,7 @@ global main
 extern printf
 
 section .data
-	quine db "global main%1$cextern printf%1$c%1$csection .data%1$c%2$cquine db %3$c%4$s%3$c, 0%1$c%1$c; this comment is outside the entry point routine%1$c%1$csection .text%1$cmain:%1$c%2$cxor rax, rax ; this comment is inside the entry point routine%1$c%2$clea rdi, [rel quine]%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, rdi%1$c%2$cjmp return%1$c%1$creturn:%1$c%2$csub rsp, 8%1$c%2$ccall printf wrt ..plt%1$c%2$cadd rsp, 8%1$c%2$cxor rax, rax%1$c%2$cret", 0
+	quine db "global main%1$cextern printf%1$c%1$csection .data%1$c%2$cquine db %3$c%4$s%3$c, 0%1$c%1$c; this comment is outside the entry point routine%1$c%1$csection .text%1$cmain:%1$c%2$cxor rax, rax ; this comment is inside the entry point routine%1$c%2$clea rdi, [rel quine]%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, rdi%1$c%2$csub rsp, 8%1$c%2$ccall print_str%1$c%2$cadd rsp, 8%1$c%2$cxor rax, rax%1$c%2$cret%1$c%1$cprint_str:%1$c%2$csub rsp, 8%1$c%2$ccall printf wrt ..plt%1$c%2$cadd rsp, 8%1$c%2$cret", 0
 
 ; this comment is outside the entry point routine
 
@@ -14,11 +14,14 @@ main:
 	mov rdx, 9
 	mov rcx, 34
 	mov r8, rdi
-	jmp return
+	sub rsp, 8
+	call print_str
+	add rsp, 8
+	xor rax, rax
+	ret
 
-return:
+print_str:
 	sub rsp, 8
 	call printf wrt ..plt
 	add rsp, 8
-	xor rax, rax
 	ret
